1ab9fba81ed2ac217e253478d8e4eba2f035645b
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-10-11 15:25:36 +0800

    feat: 加上了defer的测试

--- script_module/model2.mjs
+++ script_module/model2.mjs
@@ -1,5 +1,14 @@
+/*
+ * @Name: 
+ * @Description: 
+ * @Author: 刘燕保
+ * @Date: 2020-10-10 19:26:54
+ */
+import { subfun } from './submodule.mjs'
+
 export const modelTemp = 239
 
 export const modelCon = () => {
-    console.log('重新启动m2')
+    console.log('调用module2方法')
+    subfun()
 }
---
+++ script_module/submodule.mjs
@@ -0,0 +1,11 @@
+/*
+ * @Name: 
+ * @Description: 
+ * @Author: 刘燕保
+ * @Date: 2020-10-10 19:40:04
+ */
+console.log('引入了子模块')
+
+export const subfun = () => {
+  console.log('调用了子模块subfun方法')
+}
