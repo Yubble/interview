6aea1ef1d629c68388e8e4102b152b39981642cc
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-11-01 12:04:25 +0800

    feat: 整理代码

--- script_module/model1.mjs
+++ script_module/model1.mjs
@@ -1,5 +1,11 @@
+/*
+ * @Name: 
+ * @Description: 
+ * @Author: 刘燕保
+ * @Date: 2020-10-10 19:26:54
+ */
 
-console.log('执行了modle')
+console.log('执行了modle1')
 
 export default () => {
     console.log('执行了modle1方法')
